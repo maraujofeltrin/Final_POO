27a78e4f3b4f1e48388da051e7f7dbdcb274512d
maraujofeltrin <manuel.araujofeltrin@gmail.com>
2024-07-01 12:42:31 -0300

    Modificacion a circle y square

--- source/backend/model/Circle.java
+++ source/backend/model/Circle.java
@@ -1,26 +1,20 @@
 package backend.model;
 
-public class Circle implements Figure {
+public class Circle extends Ellipse {
 
-    protected final Point centerPoint;
-    protected final double radius;
 
     public Circle(Point centerPoint, double radius) {
-        this.centerPoint = centerPoint;
-        this.radius = radius;
+        super(centerPoint, 2 * radius, 2*radius);
     }
 
     @Override
     public String toString() {
-        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, radius);
+        return String.format("Círculo [Centro: %s, Radio: %.2f]", centerPoint, getRadius());
     }
 
-    public Point getCenterPoint() {
-        return centerPoint;
-    }
 
     public double getRadius() {
-        return radius;
+        return getsMayorAxis()/2;
     }
 
 }
